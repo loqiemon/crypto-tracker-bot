4e217d2f8be7be834d74dd93c47b746fb2d95ece
danil <d.luferenko@yandex.ru>
2026-05-12 23:35:21 +0300

    cache

--- db/base.py
+++ db/base.py
@@ -1,5 +1,5 @@
 from sqlalchemy.ext.asyncio import AsyncSession, async_sessionmaker, create_async_engine
-
+from sqlalchemy.pool import NullPool
 from config import settings
 
 engine = create_async_engine(
@@ -7,6 +7,11 @@ engine = create_async_engine(
     echo=True,
     pool_size=5,
     max_overflow=10,
+    connect_args={
+        "prepared_statement_cache_size": 0,
+        "statement_cache_size": 0,
+    },
+    poolclass=NullPool,
 )
 
 async_session_maker = async_sessionmaker(
